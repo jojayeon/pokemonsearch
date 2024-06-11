2e77f1e816fe77589366314da60de6d352b935c7
jojayeon <jojayeon6152@gmail.com>
2024-06-11 18:04:01 +0900

    서버 만들기 시작

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-var XMLHttpRequest = require('xhr2'); // - 불러와서 해보기 
+const XMLHttpRequest = require('xhr2'); // - 불러와서 해보기
+const xml = new XMLHttpRequest();
+const http = require("http")
+const fs = require("fs");
+
+const server = http.createServer((req,res)=> {
+  if(req.method === "GET" && req.url === "/") {
+    
+  }
+});
+
+
